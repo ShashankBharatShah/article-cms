97b0b928c72efc97c603c73c9fce4a5c140ceeb8
Shashank Bharat Shah <125245000+ShashankBharatShah@users.noreply.github.com>
2026-03-13 23:34:41 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -8,7 +8,7 @@ from werkzeug.urls import url_parse
 from config import Config
 from FlaskWebProject import app, db
 from FlaskWebProject.forms import LoginForm, PostForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
 import msal
 import uuid
@@ -17,9 +17,9 @@ imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + '.blob.core.windows.n
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
-    # Temporary: avoid DB query to prevent SQL connection error
-    posts = []
+    posts = Post.query.all()
     return render_template(
         'index.html',
         title='Home Page',
@@ -27,26 +27,62 @@ def home():
     )
 
 @app.route('/new_post', methods=['GET', 'POST'])
+@login_required
 def new_post():
-    # Temporary: disable DB write
-    form = PostForm(request.form)
-    return redirect(url_for('home'))
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post()
+        post.save_changes(form, request.files['image_path'], current_user.id, new=True)
+        return redirect(url_for('home'))
+
+    return render_template(
+        'post.html',
+        title='Create Post',
+        imageSource=imageSourceUrl,
+        form=form
+    )
 
 @app.route('/post/<int:id>', methods=['GET', 'POST'])
+@login_required
 def post(id):
-    # Temporary: disable DB edit
-    return redirect(url_for('home'))
+    post = Post.query.get(int(id))
+    form = PostForm(obj=post)
+
+    if form.validate_on_submit():
+        post.save_changes(form, request.files['image_path'], current_user.id)
+        return redirect(url_for('home'))
+
+    return render_template(
+        'post.html',
+        title='Edit Post',
+        imageSource=imageSourceUrl,
+        form=form
+    )
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Temporary login bypass (no DB authentication)
-    form = LoginForm()
-
-    if request.method == 'POST':
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
 
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+
+        return redirect(next_page)
+
     session["state"] = str(uuid.uuid4())
-    auth_url = ""
+    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
 
     return render_template(
         'login.html',
@@ -58,22 +94,43 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    session.clear()
+
+    if session.get("user"):
+        session.clear()
+        return redirect(
+            Config.AUTHORITY + "/oauth2/v2.0/logout" +
+            "?post_logout_redirect_uri=" + url_for("login", _external=True)
+        )
+
     return redirect(url_for('login'))
 
 @app.route(Config.REDIRECT_PATH)
 def authorized():
+
+    if request.args.get('state') != session.get("state"):
+        return redirect(url_for("home"))
+
+    if "error" in request.args:
+        return render_template("auth_error.html", result=request.args)
+
+    if request.args.get('code'):
+        user = User.query.filter_by(username="admin").first()
+        login_user(user)
+
     return redirect(url_for('home'))
 
+
 def _load_cache():
-    cache = None
-    return cache
+    return None
+
 
 def _save_cache(cache):
     pass
 
+
 def _build_msal_app(cache=None, authority=None):
     return None
+
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     return ""
